553a916da5471aeeefa073d2b5c41d646db43e90
k21rs094 <k21rs094@st.kyusan-u.ac.jp>
2022-12-11 07:51:11 +0900

    Project's Initial Commit

---
+++ www/js/app.js
@@ -0,0 +1,65 @@
+var ncmb = new NCMB(appKey, clientKey);
+
+///// Called when app launch
+$(function() {
+    $.mobile.defaultPageTransition = 'none';
+    $("#LoginBtn").click(onLoginBtn);
+    $("#RegisterBtn").click(onRegisterBtn);
+    $("#YesBtn_logout").click(onLogoutBtn);
+});
+
+//----------------------------------USER MANAGEMENT-------------------------------------//
+var currentLoginUser; //現在ログイン中ユーザー
+
+function onRegisterBtn()
+{
+    //入力フォームからusername, password変数にセット
+    var username = $("#reg_username").val();
+    var password = $("#reg_password").val();
+    
+    var user = new ncmb.User();
+    user.set("userName", username)
+        .set("password", password);
+    
+    // ユーザー名とパスワードで新規登録
+    user.signUpByAccount()
+        .then(function(reg_user) {
+            // 新規登録したユーザーでログイン
+            ncmb.User.login(reg_user)
+                     .then(function(login_user) {
+                         alert("新規登録とログイン成功");
+                         currentLoginUser = ncmb.User.getCurrentUser();
+                     　  $.mobile.changePage('#DetailPage');
+                     })
+                     .catch(function(error) {
+                         alert("ログイン失敗！次のエラー発生: " + error);
+                     });
+        })
+        .catch(function(error) {
+            alert("新規登録に失敗！次のエラー発生：" + error);
+        });
+}
+
+function onLoginBtn()
+{
+    var username = $("#login_username").val();
+    var password = $("#login_password").val();
+    // ユーザー名とパスワードでログイン
+    ncmb.User.login(username, password)
+        .then(function(user) {
+            alert("ログイン成功");
+            currentLoginUser = ncmb.User.getCurrentUser();
+            $.mobile.changePage('#DetailPage');
+        })
+        .catch(function(error) {
+            alert("ログイン失敗！次のエラー発生: " + error);
+        });
+}
+
+function onLogoutBtn()
+{
+    ncmb.User.logout();
+    alert('ログアウト成功');
+    currentLoginUser = null;
+    $.mobile.changePage('#LoginPage');
+}
